a1087c13471c413dfa219a8d5067e1f85b7bab08
Sylvain Bouchard <sylvain.bouchard@live.ca>
2025-12-13 15:06:34 -0500

    feat: add MqttService to application state

--- src/state.rs
+++ src/state.rs
@@ -2,9 +2,11 @@ use std::sync::Arc;
 
 use crate::api::vent::vent_controller::VentApiController;
 use crate::services::vent_service::VentService;
+use crate::services::mqtt_service::MqttService;
 
 #[derive(Clone)]
 pub struct ApplicationState {
     pub vent_service: Arc<VentService>,
     pub vent_api_controller: Arc<VentApiController>,
+    pub mqtt_service: Arc<MqttService>,
 }
